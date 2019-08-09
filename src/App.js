11102f34a9169e3778290f2f16aa7b05f7f8aaec
simarjeet <simarjeet.rajput@teamlance.io>
2019-08-09 12:50:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const state = {
+  territories: [
+    {
+      id: 1,
+      number: 1,
+      title: "Territory 1",
+      streets: [
+        {
+          id: 1,
+          name: "Name of street 1",
+          houses: [
+            {
+              id: 1,
+              number: "11"
+            },
+            {
+              id: 2,
+              number: "689"
+            },
+            {
+              id: 3,
+              number: "117a"
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "Name of street 2",
+          houses: [
+            {
+              id: 4,
+              number: "02"
+            },
+            {
+              id: 5,
+              number: "655"
+            },
+            {
+              id: 6,
+              number: "11b"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return state.territories.map(territory => {
+    return territory.streets.map(street => {
+      return (
+        <div key={street.id} className="App">
+          <p>{street.name}</p>
+          {street.houses.map(house => <div key={house.id} >{house.number} </div>)}
+        </div>
+      );
+    });
+  });
 }
 
 export default App;
